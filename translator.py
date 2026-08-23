22480994c83ebfce0f3c053d7c578dc0b4af01c6
k0533138501-sketch <k0533138501@gmail.com>
2026-08-23 22:53:14 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -87,22 +87,22 @@ def yemot():
         call_id = data.get("ApiCallId", "")
         he_ru_mode = request.path == "/he-ru"
         if data.get("Replay") == "1" and call_id in last_translations:
-        saved = last_translations[call_id]
-        recording = saved["recording"]
-        translation = saved["translation"]
-
-        play_path = recording
-
-        if play_path.startswith("ivr2:"):
-            play_path = play_path[5:]
-
-        if play_path.lower().endswith(".wav"):
-            play_path = play_path[:-4]
-
-        return Response(
-            f"read=f-{play_path}.t-{translation}=Replay,,1,1,20,No",
-            mimetype="text/plain"
-        )
+            saved = last_translations[call_id]
+            recording = saved["recording"]
+            translation = saved["translation"]
+    
+            play_path = recording
+    
+            if play_path.startswith("ivr2:"):
+                play_path = play_path[5:]
+    
+            if play_path.lower().endswith(".wav"):
+                play_path = play_path[:-4]
+    
+            return Response(
+                f"read=f-{play_path}.t-{translation}=Replay,,1,1,20,No",
+                mimetype="text/plain"
+            )
     if data.get("Replay") == "2":
         return Response(
             "go_to_folder=/2",
