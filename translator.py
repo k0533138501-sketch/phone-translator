0cf12821f692a7d49b44bf23e2d57abab3f31757
k0533138501-sketch <k0533138501@gmail.com>
2026-08-24 17:55:43 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -220,7 +220,7 @@ def yemot():
             }
 
         return Response(
-            f"id_list_message=f-000&" if he_ru_mode else f"read=t-{translation}=Replay,,1,1,20,No",
+            f"id_list_message=l-/10/1.f-000&" if he_ru_mode else f"read=t-{translation}=Replay,,1,1,20,No",
             mimetype="text/plain"
         )
 
