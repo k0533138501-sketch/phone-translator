2173d499e907f43580b35da623e662ed8118a1ff
k0533138501-sketch <k0533138501@gmail.com>
2026-08-25 16:45:30 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -197,12 +197,12 @@ if data.get("Study") == "1":
             f"read=f-{play_path}.t-{translation}=Study,,1,1,20,No",
             mimetype="text/plain"
         ) 
-    if data.get("Study") == "0":
-    study_positions.pop(call_id, None)
-    return Response(
-        "go_to_folder=/",
-        mimetype="text/plain"
-    )       
+      if data.get("Study") == "0":
+        study_positions.pop(call_id, None)
+        return Response(
+            "go_to_folder=/",
+            mimetype="text/plain"
+        )
     if data.get("Replay") == "1" and call_id in last_translations:
         saved = last_translations[call_id]
         recording = saved["recording"]
