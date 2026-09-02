25b83244c3376c74d115304703619ce78e5cdf68
k0533138501-sketch <k0533138501@gmail.com>
2026-09-02 12:41:26 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -411,10 +411,15 @@ def generate_one_voice():
 
     tts_path = create_russian_system_tts(messages[file_name])
 
-    result = upload_system_voice_to_yemot(
-        tts_path,
-        f"ivr2:/99/{file_name}.wav"
-    )
+if file_name == "M1000":
+    yemot_path = "ivr2:/M1000.wav"
+else:
+    yemot_path = f"ivr2:/99/{file_name}.wav"
+
+result = upload_system_voice_to_yemot(
+    tts_path,
+    yemot_path
+)
 
     return Response(result, mimetype="text/plain")    
 @app.route("/generate-number-voice", methods=["GET"])
